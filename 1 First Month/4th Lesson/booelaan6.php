503f58b92a975cb50c688c6b7266d50b284e6c58
unknown <mutaliyevutkirbek@gmail.com>
2021-10-04 03:09:51 -0700

    boolean

---
+++ 1 First Month/4th Lesson/booelaan6.php
@@ -0,0 +1,13 @@
+<?php
+$a=2;
+$b=10;
+$c=6;
+if ($a<=$b and $b<=$c and $a<=$c) {
+    echo "true";
+} else{
+    echo "false";
+}
+
+
+
+?>
---
+++ 1 First Month/4th Lesson/boolean2.php
@@ -0,0 +1,9 @@
+<?php
+$a=7;
+if ($a%2==1) {
+    echo "Bu toq son!";
+}else{
+    echo "Bu toq son emas!";
+}
+
+?>
